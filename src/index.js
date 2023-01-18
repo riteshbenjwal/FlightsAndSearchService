a72250b735c2303b90f82a54b9af681ec577c49b
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-01-18 21:17:26 +0530

    orm setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const { PORT } = require("./config/serverConfig");
 
 const setupAndStartServer = async () => {
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   const PORT = 3000;
   app.listen(PORT, () => {
     console.log(`server started at ${PORT}`);

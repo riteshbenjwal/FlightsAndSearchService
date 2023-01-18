58596d9fd2d7c35a2b312549b9f818e6e6f138ec
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-01-18 20:46:32 +0530

    project-setup

---
+++ README.md
@@ -0,0 +1,13 @@
+/
+-src/
+index.js //server
+models/
+controllers/
+middlewares/
+service/
+utils/
+config/
+
+tests/ [later]
+static/
+temp/
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
+  const app = express();
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`server started at ${PORT}`);
+  });
+};
+
+setupAndStartServer();

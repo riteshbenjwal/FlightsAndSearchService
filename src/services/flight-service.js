f91ec98b2ddc4511bd185f41eb9095897b81d0ff
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-01-29 09:07:41 +0530

    create flight setup

---
+++ src/services/flight-service.js
@@ -0,0 +1,27 @@
+const { FlightRepository, AirplaneRepository } = require("../repository/index");
+
+class FlightService {
+  constructor() {
+    this.airplaneRepository = new AirplaneRepository();
+    this.flightRepository = new FlightRepository();
+  }
+  async createFlight(data) {
+    try {
+      const airplane = await this.airplaneRepository.getAirplane(
+        data.airplaneId
+      );
+      const flight = await this.flightRepository.createFlight({
+        ...data,
+        totalSeats: airplane.capacity,
+      });
+      return flight;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getFlightData(id) {}
+}
+
+module.exports = FlightService;
